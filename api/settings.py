7fc609aeb10665732a2cd8990b467cc53afd2342
mugdha273 <mugdhasharma0327@gmail.com>
2023-03-11 08:05:07 +0530

    fix

--- api/settings.py
+++ api/settings.py
@@ -27,9 +27,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "http://localhost:3000", "https://after-ease.netlify.app/","http://localhost:8000", "https://after-ease.onrender.com/" ]
+ALLOWED_HOSTS = [ "http://localhost:3000", "https://after-ease.netlify.app","http://localhost:8000", "https://after-ease.onrender.com" ]
 
-CORS_ALLOWED_ORIGINS = [ 'http://localhost:3000', "https://after-ease.netlify.app/"]
+CORS_ALLOWED_ORIGINS = [ 'http://localhost:3000', "https://after-ease.netlify.app"]
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000", "https://after-ease.netlify.app/"
