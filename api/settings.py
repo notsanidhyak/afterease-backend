de5883214c765d973f3ae92751c9ac286289e26b
mugdha273 <mugdhasharma0327@gmail.com>
2023-03-11 08:13:42 +0530

    allowed all

--- api/settings.py
+++ api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ "http://localhost:3000", "https://after-ease.netlify.app","http://localhost:8000", "https://after-ease.onrender.com", "https://smart-identity-platform.onrender.com" ]
+ALLOWED_HOSTS = [ "*" ]
 
 CORS_ALLOWED_ORIGINS = [ 'http://localhost:3000', "https://after-ease.netlify.app"]
 
